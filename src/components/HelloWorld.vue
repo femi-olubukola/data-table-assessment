7dd355fc13a1c428c9c26e03328de64c8c8e4a1d
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-02-23 20:41:01 +0100

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,184 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="background flex px-16 py-10">
+
+    <div>
+      <h2 style="color: #8B84BA">TABLE HEADING</h2>
+
+      <v-card class="second-background">
+        <div class="d-flex justify-space-between px-4 py-4">
+
+          <div class="d-flex">
+            <div class="pr-5">
+              <v-btn prepend-inner-icon="mdi-filter"  outlined style="color: #8B84BA; height: 42px; width: 80px" text-color="red" class="text-capitalize" >
+              <span >
+                <v-icon style="color: #8B84BA">
+                  mdi-filter
+                </v-icon>
+              </span>
+                Filter
+              </v-btn>
+            </div>
+
+            <div class="field">
+              <v-text-field background-color="#F2F0F9" prepend-inner-icon="mdi-magnify" hide-details="auto" outlined dense color="grey" class="text-field" placeholder="Search Users By Name, Email Or Date">
+
+              </v-text-field>
+            </div>
+          </div>
+
+          <div class="pl-4">
+            <v-btn outlined class="white--text" style="color: mediumpurple; background-color: mediumpurple">
+              Pay Dues
+            </v-btn>
+          </div>
+
+        </div>
+
+        <div class="header-background">
+
+        </div>
+
+      </v-card>
+    </div>
+<!--    <v-row >-->
+
+<!--    </v-row>-->
+<!--    <v-row class="text-center">-->
+<!--      <v-col cols="12">-->
+<!--        <v-img-->
+<!--          :src="require('../assets/logo.svg')"-->
+<!--          class="my-3"-->
+<!--          contain-->
+<!--          height="200"-->
+<!--        />-->
+<!--      </v-col>-->
+
+<!--      <v-col class="mb-4">-->
+<!--        <h1 class="display-2 font-weight-bold mb-3">-->
+<!--          Welcome to Vuetify-->
+<!--        </h1>-->
+
+<!--        <p class="subheading font-weight-regular">-->
+<!--          For help and collaboration with other Vuetify developers,-->
+<!--          <br>please join our online-->
+<!--          <a-->
+<!--            href="https://community.vuetifyjs.com"-->
+<!--            target="_blank"-->
+<!--          >Discord Community</a>-->
+<!--        </p>-->
+<!--      </v-col>-->
+
+<!--      <v-col-->
+<!--        class="mb-5"-->
+<!--        cols="12"-->
+<!--      >-->
+<!--        <h2 class="headline font-weight-bold mb-3">-->
+<!--          What's next?-->
+<!--        </h2>-->
+
+<!--        <v-row justify="center">-->
+<!--          <a-->
+<!--            v-for="(next, i) in whatsNext"-->
+<!--            :key="i"-->
+<!--            :href="next.href"-->
+<!--            class="subheading mx-3"-->
+<!--            target="_blank"-->
+<!--          >-->
+<!--            {{ next.text }}-->
+<!--          </a>-->
+<!--        </v-row>-->
+<!--      </v-col>-->
+
+<!--      <v-col-->
+<!--        class="mb-5"-->
+<!--        cols="12"-->
+<!--      >-->
+<!--        <h2 class="headline font-weight-bold mb-3">-->
+<!--          Important Links-->
+<!--        </h2>-->
+
+<!--        <v-row justify="center">-->
+<!--          <a-->
+<!--            v-for="(link, i) in importantLinks"-->
+<!--            :key="i"-->
+<!--            :href="link.href"-->
+<!--            class="subheading mx-3"-->
+<!--            target="_blank"-->
+<!--          >-->
+<!--            {{ link.text }}-->
+<!--          </a>-->
+<!--        </v-row>-->
+<!--      </v-col>-->
+
+<!--      <v-col-->
+<!--        class="mb-5"-->
+<!--        cols="12"-->
+<!--      >-->
+<!--        <h2 class="headline font-weight-bold mb-3">-->
+<!--          Ecosystem-->
+<!--        </h2>-->
+
+<!--        <v-row justify="center">-->
+<!--          <a-->
+<!--            v-for="(eco, i) in ecosystem"-->
+<!--            :key="i"-->
+<!--            :href="eco.href"-->
+<!--            class="subheading mx-3"-->
+<!--            target="_blank"-->
+<!--          >-->
+<!--            {{ eco.text }}-->
+<!--          </a>-->
+<!--        </v-row>-->
+<!--      </v-col>-->
+<!--    </v-row>-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+
+    }),
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.background {
+  background-color: #F2F0F9;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.second-background {
+  height: 100vh;
+  border-radius: 12px;
+  color: #ffffff;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.text-field {
+  width: 600px;
+  height: 39px;
 }
-a {
-  color: #42b983;
+
+.header-background {
+  background-color: #F2F0F9;
+  height: 8vh;
+  //width: 1000px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
 }
+
+
+
 </style>
